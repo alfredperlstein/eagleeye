48ee40fea146082fa2bb863e453cd49dd4c9c011
Alfred Perlstein <alfred@freebsd.org>
2013-05-22 23:28:44 -0400

    Clean more from old broken sysctl scraper.
    deal with "|foo bar: 232|" which is an error.
    
    add some comments while here.

--- src/fixup_bad_sysctl_all.sh
+++ src/fixup_bad_sysctl_all.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-# fixup unparseable sysctl logs.
+# fixup bad sysctl logs grabbed from old version of eagleeye
+
+#
+# If we are given arguments, then act on each argument as a filename
+# to manipulate instead of a pipe.
+#
+if [ "x$1" != "x" ] ; then
+    INPLACE="-i .bak"
+fi
 
 # the old regex for sysctl pruning was letting in bad strings
 # such as "<118> syncpeer: 0.0.0.0 maxupd: 128", use this
 # sed to strip it out.
 
-if [ "x$1" != "x" ] ; then
-    INPLACE="-i .bak"
-fi
-
-sed $INPLACE -e 's/|[^a-z][^|]*//g' -e 's/|[^|:]*: [^0-9][^|]*//g' $*
+# 1st pattern: replace any column that doesn't start with [a-z]
+# 2nd pattern: replace any column that has non numeric data
+# 3rd pattern: replace any column with multiple occurences of whitespace
+sed $INPLACE \
+     -e 's/|[^a-z][^|]*//g' \
+     -e 's/|[^|:]*: [^0-9][^|]*//g' \
+     -e 's/|[^| ]* [^| ]* [^|]*//g' \
+     $*
 
