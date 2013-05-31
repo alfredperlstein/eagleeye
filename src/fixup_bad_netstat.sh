e98c568b4df38b6fb58dd8e822080f4d065ae076
Alfred Perlstein <alfred@freebsd.org>
2013-05-30 20:57:15 -0700

    Deal with badly captured netstat data.

---
+++ src/fixup_bad_netstat.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# fixup bad sysctl logs grabbed from old version of eagleeye
+
+#
+# If we are given arguments, then act on each argument as a filename
+# to manipulate instead of a pipe.
+#
+if [ "x$1" != "x" ] ; then
+    INPLACE="-i .bak"
+fi
+
+# the old regex for sysctl pruning was letting in bad strings
+# such as "<118> syncpeer: 0.0.0.0 maxupd: 128", use this
+# sed to strip it out.
+
+# 1st pattern: replace any column that doesn't start with [a-z]
+# 2nd pattern: replace any column that has non numeric data
+# 3rd pattern: replace any column with multiple occurences of whitespace
+sed $INPLACE \
+    -e 's/\([a-z_][a-z_]*:\)/|\1/g' \
+     $*
+
