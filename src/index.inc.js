f1a227fe2bf067df93b02ecbd24bd911dee91a4e
Alfred Perlstein <alfred@freebsd.org>
2013-06-01 12:17:17 -0700

    Allow hiding/unhiding of graphs.

--- src/index.inc.js
+++ src/index.inc.js
@@ -1,6 +1,24 @@
-for (var i = 0; i < allDivs.length; i++) {
-    $('#test').append(allDivs[i] + "<br>");
+function handleCheckbox(cb) {
+//display("Clicked, new value = " + cb.checked + " name = " + cb.name);
+  divName = "#" + cb.name;
+  if (cb.checked) {
+      $(divName).hide();
+  } else {
+      $(divName).show();
+  }
+  //alert("Clicked, new value = " + cb.checked + " name = " + cb.name);
 }
+
+for (var i = 0; i < allDivs.length; i++) {
+    var cDiv = allDivs[i];
+    $('#test').append(
+		      '<label><input type=\'checkbox\' name=\'' +
+		      cDiv +
+		      '\' onclick=\'handleCheckbox(this);\'>' +
+		      //'\' onclick=\'alert(this.name);\'>' +
+		      cDiv + '</label> | ');
+}
+
 <!--	    <script type="text/javascript">
 $("hideme").hide();
 -->
