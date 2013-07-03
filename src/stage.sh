93d4f7726f4dcbc4b4fdfc25b49ea6b788de1d50
Alfred Perlstein <alfred@freebsd.org>
2013-07-02 18:48:52 -0700

    mydir trick so we can run this from another dir

--- src/stage.sh
+++ src/stage.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mydir=`dirname $0`
 dir=$1
 if [ "x$dir" = "x" ] ; then
     echo "usage: $0 <dir>"
@@ -9,4 +10,7 @@ fi
 
 mkdir -p $dir
 
-cp *.html *.js *.css *.png $dir
+cp -v index.html large.html $dir
+cp -v *.png $dir
+cp -v "${mydir}"/*.{js,css} $dir
+
